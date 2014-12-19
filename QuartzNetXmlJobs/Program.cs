99b04319b9c29089a13b0d1627b12777389620de
Alberto Basile <vandalo72@gmail.com>
2014-12-19 13:00:07 +0100

    Quartz.Net - Job from Xml
    Quartz.Net loading job from an XML file.

---
+++ QuartzNetXmlJobs/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzNetXmlJobs
+{
+    using Quartz;
+    using Quartz.Impl;
+    using Quartz.Impl.Matchers;
+    using Quartz.Simpl;
+    using Quartz.Spi;
+    using Quartz.Xml;
+    using Quartz.Xml.JobSchedulingData20;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    using Common.Logging;
+    using System.Collections.Specialized;
+
+    class Program
+    {
+        public static StdSchedulerFactory SchedulerFactory;
+        public static IScheduler Scheduler;
+
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger(typeof(Program));
+
+            SchedulerFactory = new StdSchedulerFactory();
+            Scheduler = SchedulerFactory.GetScheduler();
+
+            Scheduler.Start();
+
+            Console.WriteLine("Quartz.Net Started ...");
+
+            Console.WriteLine("***** FETCHING JOBS *****");
+
+            GetAllJobs(Scheduler);
+
+            Console.WriteLine("******************");
+
+            Console.ReadLine();
+        }
+
+       
+        private static void GetAllJobs(IScheduler scheduler)
+        {
+            int countJobs = 0;
+            IList<string> jobGroups = scheduler.GetJobGroupNames();
+            IList<string> triggerGroups = scheduler.GetTriggerGroupNames();
+
+            foreach (string group in jobGroups)
+            {
+                countJobs++;
+
+                Console.WriteLine(string.Format("----- JOB #{0} -----", countJobs.ToString()));
+
+                var groupMatcher = GroupMatcher<JobKey>.GroupContains(group);
+                var jobKeys = scheduler.GetJobKeys(groupMatcher);
+                foreach (var jobKey in jobKeys)
+                {
+                    var detail = scheduler.GetJobDetail(jobKey);
+                    var triggers = scheduler.GetTriggersOfJob(jobKey);
+                    foreach (ITrigger trigger in triggers)
+                    {
+                        Console.WriteLine("JOB GROUP: " + group);
+                        Console.WriteLine("JOB NAME: " + jobKey.Name);
+                        Console.WriteLine("JOB DESCRIPTION: " + detail.Description);
+                        Console.WriteLine("TRIGGER NAME: " + trigger.Key.Name);
+                        Console.WriteLine("TRIGGER GROUP: " + trigger.Key.Group);
+                        Console.WriteLine("TRIGGER TYPE: " + trigger.GetType().Name);
+                        Console.WriteLine("TRIGGER STATE: " + scheduler.GetTriggerState(trigger.Key));
+                        DateTimeOffset? nextFireTime = trigger.GetNextFireTimeUtc();
+                        if (nextFireTime.HasValue)
+                        {
+                            Console.WriteLine("NEXT FIRE TIME: " + nextFireTime.Value.LocalDateTime.ToString());
+                        }
+
+                        DateTimeOffset? previousFireTime = trigger.GetPreviousFireTimeUtc();
+                        if (previousFireTime.HasValue)
+                        {
+                            Console.WriteLine("PREVIOUS FIRE TIME: " + previousFireTime.Value.LocalDateTime.ToString());
+                        }
+                    }
+                }
+
+                Console.WriteLine(string.Format("/----- JOB #{0} -----/", countJobs.ToString()));
+            }
+        }
+    }
+}
---
+++ QuartzNetXmlJobs/SimpleJob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzNetXmlJobs
+{
+    using Quartz;
+    using Common.Logging;
+
+    public class SimpleJob : IJob
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(SimpleJob));
+
+        /// <summary>
+        /// Called by the <see cref="IScheduler" /> when a
+        /// <see cref="ITrigger" /> fires that is associated with
+        /// the <see cref="IJob" />.
+        /// </summary>
+        public virtual void Execute(IJobExecutionContext context)
+        {
+            // This job simply prints out its job name and the
+            // date and time that it is running
+            JobKey jobKey = context.JobDetail.Key;
+            log.InfoFormat("Executing job: {0} executing at {1}", jobKey, DateTime.Now.ToString("r"));
+
+            if (context.MergedJobDataMap.Count > 0)
+            {
+                ICollection<string> keys = context.MergedJobDataMap.Keys;
+                foreach (string key in keys)
+                {
+                    String val = context.MergedJobDataMap.GetString(key);
+                    log.InfoFormat(" - jobDataMap entry: {0} = {1}", key, val);
+                }
+            }
+
+            context.Result = "hello";
+        }
+    }
+}
